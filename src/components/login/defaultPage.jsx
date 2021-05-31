81bf30c72569482cd4a430891cd4af9ff95719f5
Gurpreetkestone <39495450+Gurpreetkestone@users.noreply.github.com>
2021-05-31 21:55:28 +0530

    es
    es

--- src/components/login/defaultPage.jsx
+++ src/components/login/defaultPage.jsx
@@ -1,13 +1,21 @@
 import NavBar from "./../navBar/NavBar";
 import MainSpace from "./../mainSpace/mainSpace";
 import SideBar from "./../sideBar/sideBar";
+import EventSettingsMain from "../globalSettings/eventSettings/eventSettingsMain";
+import { Route } from "react-router-dom";
 const DefaultPage = () => {
   return (
     <div className="paddingForHeader">
       <NavBar></NavBar>
       <main className="main" id="main">
         <SideBar></SideBar>
-        <MainSpace></MainSpace>
+        <div className="mainPart transiton">
+          <div className="container-fluid">
+            <div className="row px-2">
+              <EventSettingsMain />
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
